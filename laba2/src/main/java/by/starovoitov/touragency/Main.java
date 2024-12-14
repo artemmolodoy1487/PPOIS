8d33cdacfb54e7ec9b5bcec950273a4dfe565f6f
Артем <147653635+artemmolodoy1487@users.noreply.github.com>
2024-12-14 13:39:44 +0300

    Delete laba2 directory

--- laba2/src/main/java/by/starovoitov/touragency/Main.java
+++
@@ -1,7 +0,0 @@
-package by.starovoitov.touragency;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Tour Agency Management System is running!");
-    }
-}
